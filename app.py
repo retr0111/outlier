3f21adbc29770fdcf10f0aef704a6058309cb31e
retr0111 <40544860+retr0111@users.noreply.github.com>
2024-04-08 21:13:33 -0700

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import streamlit as st
 from wv import Model
 from scipy.stats import zscore
 
-# Load Word2Vec model
 model = Model("models/glove_short.txt")
 
 st.title("Remove Outliers from a list of words")
@@ -17,13 +16,8 @@ if words:
     if len(words) < 3:
         st.error("Please enter at least 3 words.")
     else:
-        # Filter out words with no vector representation in the model
         filtered_words = [word.strip() for word in words if word.strip() in model]
-
-        # Calculate z-scores for the vectors of the filtered words
         z_scores = zscore([model[word] for word in filtered_words])
-
-        # Remove outliers based on z-scores
         filtered_words = [filtered_words[i] for i, z_score in enumerate(z_scores) if abs(z_score) < 3]
 
         st.success(f"With outliers removed, your list looks like this: {', '.join(filtered_words)}")
